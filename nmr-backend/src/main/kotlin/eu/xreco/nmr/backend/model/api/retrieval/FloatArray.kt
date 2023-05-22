e1cc534f569542eccf2daf3db995bb8be25d6bf0
Rahel Arnold <rahel.arnold@unibas.ch>
2023-05-22 13:57:57 +0200

    added FloatArray type

--- nmr-backend/src/main/kotlin/eu/xreco/nmr/backend/model/api/retrieval/FloatArray.kt
+++ nmr-backend/src/main/kotlin/eu/xreco/nmr/backend/model/api/retrieval/FloatArray.kt
@@ -1,7 +1,3 @@
 package eu.xreco.nmr.backend.model.api.retrieval
 
-import org.vitrivr.cottontail.core.types.Types
-import org.vitrivr.cottontail.core.values.FloatVectorValue
-import org.vitrivr.cottontail.grpc.CottontailGrpc
-
-data class FloatVector(val feature: FloatVectorValue)
+data class FloatArray(val feature: FloatArray)
