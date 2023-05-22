e7e52c4c996482d1546c073c316e40ce98d665e6
Ralph Gasser <ralph.gasser@unibas.ch>
2023-05-22 16:21:04 +0200

    Added OpenAPI module to project.

--- nmr-frontend/src/app/app.module.ts
+++ nmr-frontend/src/app/app.module.ts
@@ -5,18 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {RetrievalModule} from "./components/retrieval/retrieval.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {ApiModule, Configuration} from "../../openapi";
+
+/**
+ * Provides the {@link AppConfig} reference.
+ *
+ * @param appConfig Reference (provided by DI).
+ */
+export function initializeApiConfig() {
+  return new Configuration({ basePath: "http://localhost:7070", withCredentials: true });
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    {ngModule: ApiModule, providers: [{ provide: Configuration, useFactory: initializeApiConfig }] },
+
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
 
     /* Our own modules. */
     RetrievalModule,
-     BrowserAnimationsModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
