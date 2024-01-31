0b19789ce54b2ab66a65745b599751565fd46256
Rahel Arnold <rahel.arnold@unibas.ch>
2024-01-31 15:16:55 +0100

    fix API routes

--- nmr-backend/src/main/kotlin/eu/xreco/nmr/backend/api/Routes.kt
+++ nmr-backend/src/main/kotlin/eu/xreco/nmr/backend/api/Routes.kt
@@ -7,10 +7,11 @@ import eu.xreco.nmr.backend.api.media.*
 import eu.xreco.nmr.backend.api.retrieval.*
 import eu.xreco.nmr.backend.config.Config
 import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.path
+import io.javalin.apibuilder.ApiBuilder
 import io.minio.MinioClient
 import org.vitrivr.engine.core.config.pipeline.execution.ExecutionServer
 import org.vitrivr.engine.core.model.metamodel.SchemaManager
+import io.javalin.apibuilder.ApiBuilder.path
 
 /**
  * Extension function that generates the relevant routes of the XRECO NMR backend API.
@@ -23,29 +24,29 @@ import org.vitrivr.engine.core.model.metamodel.SchemaManager
 fun Javalin.initializeRoutes(config: Config, manager: SchemaManager, runtime: ExecutionServer, minio: MinioClient): Javalin =  this.routes  {
     path("api") {
         /* Endpoints related to data ingest. */
-        post("ingest") { ingest(it, minio, manager, runtime) }
+        ApiBuilder.post("ingest") { ingest(it, minio, manager, runtime) }
         path("ingest") {
             path("{jobId}") {
-                get("status") { ingestStatus(it, manager, runtime) }
-                delete("abort") { ingestAbort(it, manager, runtime) }
+                ApiBuilder.get("status") { ingestStatus(it, manager, runtime) }
+                ApiBuilder.delete("abort") { ingestAbort(it, manager, runtime) }
             }
         }
 
         /* Endpoints related to retrieval- */
         path("retrieval") {
-            get("lookup/{elementId}/{entity}") { lookup(it, manager, runtime) }
-            get("type/{elementId}") { type(it, manager, runtime) }
-            get("text/{entity}/{text}/{pageSize}/{page}") { getFulltext(it, manager, runtime) }
-            get("similarity/{entity}/{mediaResourceId}/{timestamp}/{pageSize}/{page}") {
+            ApiBuilder.get("lookup/{elementId}/{entity}") { lookup(it, manager, runtime) }
+            ApiBuilder.get("type/{elementId}") { type(it, manager, runtime) }
+            ApiBuilder.get("text/{entity}/{text}/{pageSize}/{page}") { getFulltext(it, manager, runtime) }
+            ApiBuilder.get("similarity/{entity}/{mediaResourceId}/{timestamp}/{pageSize}/{page}") {
                 getSimilar(it, manager, runtime)
             }
-            get("filter/{condition}/{pageSize}/{page}") { filter(it) }
+            ApiBuilder.get("filter/{condition}/{pageSize}/{page}") { filter(it) }
         }
 
         /* Access to MinIO resources. */
         path("resource") {
-            get("{mediaResourceId}") { getAssetResource(it, minio) }
-            get("{mediaResourceId}/metadata") { getMetadata(it, manager, runtime) }
+            ApiBuilder.get("{mediaResourceId}") { getAssetResource(it, minio) }
+            ApiBuilder.get("{mediaResourceId}/metadata") { getMetadata(it, manager, runtime) }
         }
     }
 }
